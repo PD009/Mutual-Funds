9b1b70b65626e335ec662952210c24f466ab1c95
Priyadarshini Basker <priyadarshinib977@gmail.com>
2018-07-10 16:18:14 +0530

    db

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -21,15 +21,16 @@ const options = {
 const pgp = require('pg-promise')(options);
 // Preparing the connection details:
 const cn = {
-    host: 'ec2-54-243-235-153.compute-1.amazonaws.com',
+    host: 'ec2-54-83-59-239.compute-1.amazonaws.com',
     // host: 'postgres://karfbvsappvblx:b283af7c191ee47bcad832397a67147ac97f84677ac4172bf00d7e1935c23302@ec2-107-20-133-82.compute-1.amazonaws.com:5432/d4uk4guv9810pl',
     // host: 'localhost',    //server name or IP addres
     port: 5432,           //server port number
-    database: 'd6nig1mb5e0p6e',
+    database: 'ddvupf0ie4qkk2',
     // database: 'foodorder',       //database name
-    user: 'vzpofqklxqxnxh',     //user name
+    user: 'fkwpfbihnwfcfk',     //user name
     // user: 'postgres',     //user name
-    password: '665ca181d8e905899c6465ec345dacf2449f7c9e4764251fca69e886c6347ddf', //user password
+    password: '
+    e25bf765fe9f5b74ae89acfdd95bae41933a062f0458f824c579147fd4fafaae', //user password
     // password: 'password', //user password
     ssl: false           //use SSL
     // poolSize: 10,         //maximum size of the connection pool
