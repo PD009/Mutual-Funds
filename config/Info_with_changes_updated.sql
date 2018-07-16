3e44ab22c62841a07b4812c657efc5d053bd1872
Priyadarshini Basker <priyadarshinib977@gmail.com>
2018-07-16 10:25:19 +0530

    first 2 tables

--- config/Info_with_changes_updated.sql
+++ config/Info_with_changes_updated.sql
@@ -3,8 +3,8 @@ CREATE DATABASE info;
 
 \c info;
 
-CREATE SEQUENCE id_seq1;
-CREATE SEQUENCE id_seq2;
+--CREATE SEQUENCE id_seq1;
+--CREATE SEQUENCE id_seq2;
 CREATE SEQUENCE id_seq3;
 CREATE SEQUENCE id_seq4;
 
@@ -14,21 +14,22 @@ mobile_no       int(10)    PRIMARY KEY,  --users mobile number
 )INHERITS (assets),
 INHERITS(liabilities);*/
 
-CREATE SEQUENCE id_seq1;
+--CREATE SEQUENCE id_seq1;
 
 CREATE TABLE investment_profile_assets (
-mobileno      integer    PRIMARY KEY,  --users mobile number
-user_id integer NOT NULL DEFAULT nextval('id_seq1'),
+mobile_no      bigint    PRIMARY KEY,  --users mobile number
+--user_id integer NOT NULL DEFAULT nextval('id_seq1'),
 interest_rate         float(2),  --in percentage
-amount                  integer,   --approx net worth
+amount                  bigint,   --approx net worth
 asset_investment_date            date,  --date of the investment (MDY format)
 asset_maturity_date          date,  --date of the investment maturity (MDY format)
-time_period             varchar(10),  --time period of the investment only years,months
+typeasset            varchar(50), --types of asset investment
+time_period             timestamp,  --time period of the investment only years,months
 time_recorded           timestamp  --time when it was entered in the database
-)INHERITS (assets_types_master);
+);
 
-ALTER SEQUENCE id_seq1
-owned BY investment_profile_assets.mobile_no;
+/*ALTER SEQUENCE id_seq1
+owned BY investment_profile_assets.mobile_no;*/
 
 CREATE TABLE assets_types_master(
 typeasset            varchar(50) --types of asset investment
@@ -36,26 +37,27 @@ typeasset            varchar(50) --types of asset investment
 
 
 CREATE TABLE investment_profile_liabilities (
-mobile_no       int(10)    PRIMARY KEY,  --users mobile number
-user_id integer NOT NULL DEFAULT nextval('id_seq'),
+mobile_no       bigint   PRIMARY KEY,  --users mobile number
+--user_id integer NOT NULL DEFAULT nextval('id_seq2'),
 interest_rate     float(2),  --in percentage
-amount              int,   --approx net worth
-liability_investment_date date,  --date of the liability  (MDY format)
+amount              bigint,   --approx net worth
+liability_investment_date date,  --date of the liability  (YMD format)
 liability_maturity_date           date,  --date of the liability ends  (MDY format)
+typeliability            varchar(50), --types of asset investment
 time_period         timestamp,  --time period of the investment only years,months
 time_recorded           timestamp  --time when it was entered in the database
-)INHERITS (type_liability);
+);
 
-ALTER SEQUENCE id_seq2
-owned BY investment_profile_liabilities.mobile_no;
+/*ALTER SEQUENCE id_seq2
+owned BY investment_profile_liabilities.mobile_no;*/
 
 CREATE TABLE liabilities_types_master(
 typeliability            varchar(50) --types of asset investment
 );
 
 CREATE TABLE user_profile(
-mobile_no      int    PRIMARY KEY,  --users mobile number
-user_id integer NOT NULL DEFAULT nextval('id_seq2'),
+mobile_no      bigint    PRIMARY KEY,  --users mobile number
+user_id integer NOT NULL DEFAULT nextval('id_seq3'),
 username varchar(50),  
 --phoneno        int(8),  --user's phone number
 email_addr       varchar(20),
@@ -68,15 +70,16 @@ owned BY user_profile.mobile_no;
 
 CREATE TABLE investment_goal(
 --date_start      date,  -- starting date of the investment  (MDY format)
-mobile_no       int   PRIMARY KEY,  --users mobile number
-user_id integer NOT NULL DEFAULT nextval('id_seq3'),
+mobile_no       bigint   PRIMARY KEY,  --users mobile number
+user_id integer NOT NULL DEFAULT nextval('id_seq4'),
 goal_planned_date        date,  --ending date of the investment  (MDY format)
+typegoals            varchar(50),--types of asset investment
 time_recorded           timestamp,  --time when it was entered in the database
 --time_period             timestamp  --time period of the investment only years
 
-)INHERITS (goals);
+);
 
-ALTER SEQUENCE id_seq3
+ALTER SEQUENCE id_seq4
 owned BY user_profile.mobile_no;
 
 CREATE TABLE goals_master(
--- modals/orderQueries.js
+++ modals/orderQueries.js
@@ -6,16 +6,8 @@ var db = require('../config/dbconnection');
 function createEntry(req,res,next){
     var reqObj = req.body.queryResult.parameters;
     console.log(reqObj);
-    db.one('INSERT INTO investment_profile_assets(mobileno, interest_rate, amount, asset_investment_date, asset_maturity_date, time_period) values($1, $2, $3, $4, $5, $6)',
-    [reqObj.mobileno,reqObj.interest_rate,reqObj.amount,reqObj.asset_investment_date,reqObj.asset_maturity_date, reqObj.time_period])
-/*db.one("INSERT INTO investment_profile_assets (DEFAULT $1,$2,$3,$4,$5,$6,$7)",[
-    reqObj.mobileno,
-    reqObj.interest_rate,
-    reqObj.amount,
-    reqObj.asset_investment_date,
-    reqObj.asset_maturity_date, 
-    reqObj.typeasset,
-    reqObj.time_period])*/
+    db.one('INSERT INTO investment_profile_assets(mobile_no, interest_rate, amount, asset_investment_date, asset_maturity_date, typeasset, time_period) values($1, $2, $3, $4, $5, $6, $7)',
+    [reqObj.mobileno,reqObj.interest_rate,reqObj.amount,reqObj.asset_investment_date,reqObj.asset_maturity_date,reqObj.typeasset, reqObj.time_period])
     .then(function(result){
         var message = {status:"success",
                        message:"record inserted",
@@ -34,8 +26,8 @@ function createEntry(req,res,next){
     function createIP(req,res,next){
         var reqObj = req.body.queryIP.parameters;
         console.log(reqObj);
-        db.one('INSERT INTO investment_profile_liabilities(mobile_no, interest_rate, amount, liability_investment_date, liability_maturity_date, time_period) values($1, $2, $3, $4, $5, $6)',
-        [reqObj.mobile_no,reqObj.interest_rate,reqObj.amount,reqObj.liability_investment_date,reqObj.liability_maturity_date, reqObj.time_period])
+        db.one('INSERT INTO investment_profile_liabilities(mobile_no, interest_rate, amount, liability_investment_date, liability_maturity_date, typeliability, time_period) values($1, $2, $3, $4, $5, $6)',
+        [reqObj.mobile_no,reqObj.interest_rate,reqObj.amount,reqObj.liability_investment_date,reqObj.liability_maturity_date,reqObj.typeliability, reqObj.time_period])
         .then(function(result){
             var message = {status:"success",
                            message:"record inserted",
